c3fee16dfee11e3ebb72732c074e6897fb73cc50
secretfirefox <141532262+secretfirefox@users.noreply.github.com>
2023-09-25 12:45:36 -0400

    Add files via upload

---
+++ update-icon-cache.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script was made by Secret Firefox.
+# It works the same way as the default script in Mint-Yz packages.    
+# It really isn't necessary on window managers, but the Appearance application on Xfce needs it to work nicely.
+# Otherwise, there will be lag when loading the Icons tab in the Appearance app.
+# Enjoy!
+
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Base
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Base-Aqua
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Base-BlueBelize 
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Base-BlueElectron
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Base-Grey 
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Base-MintGum
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Base-MintSoda
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Base-Orange
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Base-Orange70s
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Base-Pink
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Base-Purple
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Base-Red
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Base-Yellow
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Dark 
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Dark-Aqua
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Dark-BlueBelize
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Dark-BlueElectron 
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Dark-Grey
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Dark-MintGum
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Dark-MintSoda
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Dark-Orange
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Dark-Orange70s
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Dark-Pink 
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Dark-Purple
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Dark-Red
+gtk-update-icon-cache /usr/share/icons/Mint-Yz-Dark-Yellow
